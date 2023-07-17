822935d069746370e2db6a2f950bd97f597d54b2
TKMshengu <tokocha81@gmail.com>
2023-07-17 18:48:12 +0200

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -15,17 +15,35 @@ def convert_markdown_tohtml(input_file, out_file):
     print(f"Missing {input_file}", file=sys.stderr)
     sys.exit(1)
 
-# Read the Markdown file and conert it to HTML
-with open(input_file, encoding="utf-8" as f:
-  html_lines = []
-  for line in f:
-    # Check for the Markdown headings
-    match = re.match(r"^(#+) (.*)$', line)
-                     if match:
-                     heading_level = len(match.group(1))
-    heading_text = match.group(2)
-    html_lines.append(f"<h{heading_level}>{heading_text}</h{heading_level}>")
-else:
-html_lines.append(line.rstrip())
-# Write the HTML output to file
-with open(output_file, "w" enconding
+# Read the Markdown file and convert it to HTML
+    with open(input_file, encoding="utf-8") as f:
+        html_lines = []
+        for line in f:
+            # Check for Markdown headings
+            match = re.match(r"^(#+) (.*)$", line)
+            if match:
+                heading_level = len(match.group(1))
+                heading_text = match.group(2)
+                html_lines.append(f"<h{heading_level}>{heading_text}</h{heading_level}>")
+            else:
+                html_lines.append(line.rstrip())
+
+# Write the HTML output to a file
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write("\n".join(html_lines))
+
+if __name__ == "__main__":
+    # Check that the correct number of arguments were provided
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
+        sys.exit(1)
+
+    # Get the input and output file names from the command-line arguments
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    # Convert the Markdown file to HTML and write the output to a file
+    convert_markdown_to_html(input_file, output_file)
+
+    # Exit with a successful status code
+    sys.exit(0)
